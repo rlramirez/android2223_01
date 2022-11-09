0a3f9dd28eae509b978d30310bbec225e7baf336
rlramirez <rlramirez656@gmail.com>
2022-11-09 16:39:39 -0500

    envio de variables entre ventanas

--- app/src/main/java/com/sabirm/android2223_01/Ventana2.kt
+++ app/src/main/java/com/sabirm/android2223_01/Ventana2.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_ventana2.*
 
 class Ventana2 : AppCompatActivity() {
@@ -15,8 +16,23 @@ class Ventana2 : AppCompatActivity() {
 
 
         btn_2.setOnClickListener{
-            val saltar:Intent=Intent(this, MainActivity::class.java)
-            startActivity(saltar)
+            try {
+                //recolectar variables del formulario
+                var nombre:String=et_nombres.text.toString()
+                var Edad:String=et_edad.text.toString()
+                var edad=Integer.parseInt(Edad) //convirtiendo a entero Edad
+
+                val saltar:Intent=Intent(this, procesar::class.java)
+                //envío de variables
+                saltar.putExtra("nombre",nombre)
+                saltar.putExtra("edad",edad)
+                startActivity(saltar)
+            } catch (e: NumberFormatException) {
+                var nombre="Sin nombre"
+                var edad = 0
+                Toast.makeText(applicationContext, "LLene el formulario por completo",
+                    Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }

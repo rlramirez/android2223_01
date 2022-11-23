40535f05a75c5da89f3171444c33b87c4de074c6
rlramirez <rlramirez656@gmail.com>
2022-11-23 16:29:19 -0500

    layouts

---
+++ app/src/main/java/com/sabirm/android2223_01/Auth_Ativity.kt
@@ -0,0 +1,11 @@
+package com.sabirm.android2223_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Auth_Ativity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth_ativity)
+    }
+}
---
+++ app/src/main/java/com/sabirm/android2223_01/LoginUser.kt
@@ -0,0 +1,11 @@
+package com.sabirm.android2223_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_user)
+    }
+}
